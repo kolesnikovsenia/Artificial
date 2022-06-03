9ee3509f363a25d1af2d9131f7660a7056e2a20f
kolesnikovsenia <kolesnikovsena718@gmail.com>
2022-06-03 13:14:26 +0300

    figma

---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+const project_folder = "dist";
+const sorce_folder = "#src";
+
+const path={
+	build:{
+		html: project_folder + "/",
+		css: project_folder + "/css/",
+		img: project_folder + "/img/",
+		js: project_folder + "/js/",
+		icons: project_folder + "/icons/",
+	},
+	src:{
+		html: [sorce_folder + "/*.html", "!"+sorce_folder + "/_*.html"],
+		css: sorce_folder + "/scss/*.scss", 
+		img: sorce_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+		js: sorce_folder + "/js/main.js",
+		icons: sorce_folder + "/icons/*.svg",
+	},
+	watch:{
+		html: sorce_folder + "/**/*.html",
+		css: sorce_folder + "/scss/**/*.scss",
+		img: sorce_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+		js: sorce_folder + "/js/main.js",
+		icons: sorce_folder + "/icons/*.svg",
+	},
+	clean: "./" + project_folder + "/"
+}
+
+const {src,dest} = require('gulp'),
+	gulp = require('gulp'),
+	browsersync = require('browser-sync').create(),
+	fileinclude = require('gulp-file-include'),
+	del = require('del'),
+	scss = require('gulp-sass') (require('sass')),
+	autoprefixer = require('gulp-autoprefixer'),
+	group_media = require('gulp-group-css-media-queries'),
+	clean_css = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify-es').default;
+
+function browserSync(params) {
+	browsersync.init({
+		server:{
+			baseDir: "./"+project_folder+"/"
+		},
+		port: 5000,
+		notify: false
+	})
+}
+
+function css() {
+	return src(path.src.css)
+		.pipe(
+			scss({
+				outputStyle: "expanded"
+			})
+		)
+		.pipe(
+			group_media()
+		)
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ["last 5 version"],
+				cascade: true
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe (clean_css())
+		.pipe(
+			rename({
+				extname: ".min.css"
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream())
+}
+
+function html() {
+	return src(path.src.html)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream())
+}
+
+
+
+function js() {
+	return src(path.src.js)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.js))
+		.pipe(uglify())
+		.pipe(
+			rename({
+				extname: ".min.js"
+			})
+		)
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream())
+}
+
+function icons() {
+	return src(path.src.icons)
+		.pipe(dest(path.build.icons))
+		.pipe(browsersync.stream())
+}
+
+function images() {
+	return src(path.src.img)
+		.pipe(dest(path.build.img))
+		.pipe(browsersync.stream())
+}
+
+function watchFiles(params) {
+	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+	gulp.watch([path.watch.js], js);
+	gulp.watch([path.watch.img], images);
+	gulp.watch([path.watch.icons], icons);
+}
+
+function clean(params) {
+	return del(path.clean);
+}
+
+const build = gulp.series(clean, gulp.parallel(css, js, html, images, icons)); 
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.images = images;
+exports.icons = icons;
+exports.js = js;
+exports.css = css;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
