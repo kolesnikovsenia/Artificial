87519e6227215c92d55bda557bb1972580aec962
kolesnikovsenia <kolesnikovsena718@gmail.com>
2022-06-13 14:53:24 +0300

    create: burger menu

--- #src/js/main.js
+++ #src/js/main.js
@@ -7,6 +7,48 @@ $(document).ready(function(){
 		infinite: true,
 		speed: 3000,
 		autoplay: true,
-		autoplaySpeed: 2000
+		autoplaySpeed: 2000,
+		responsive:[
+			{
+				breakpoint: 426,
+				settings:{
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode:true,
+				}
+			},
+			{
+				breakpoint: 321,
+				settings:{
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			}
+		]
 	});
 });
+
+const burgerButtonSelector = document.querySelector('.burger');
+const mobileMenuSelector = document.querySelector('.mobile_menu');
+const closeButtonSelector = document.querySelector('.close');
+const bodyWrapperSelector = document.querySelector('.body_wrapper');
+const mobileMenuItemSelector = document.querySelector('.link_list')
+const menuSelector = document.querySelector('.menu')
+
+
+burgerButtonSelector.addEventListener("click",()=>{
+    mobileMenuSelector.classList.add("active1")
+    bodyWrapperSelector.classList.add("active")
+})
+
+closeButtonSelector.addEventListener("click",()=>{
+    menuSelector.classList.remove("active")
+    mobileMenuSelector.classList.remove("active1")
+    bodyWrapperSelector.classList.remove("active")
+})
+
+mobileMenuItemSelector.addEventListener("click",()=>{
+    menuSelector.classList.remove("active")
+    mobileMenuSelector.classList.remove("active1")
+    bodyWrapperSelector.classList.remove("active")
+})
